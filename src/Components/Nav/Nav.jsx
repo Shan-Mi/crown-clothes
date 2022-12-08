259f60f201a1bd5ca2f99f7a9d09364fe0be7fad
Shan-Mi <shan.mi.fanfan@gmail.com>
2022-12-08 21:17:33 +0100

    Add Nav and its style
    Also include logo svg file

---
+++ src/Components/Nav/Nav.jsx
@@ -0,0 +1,44 @@
+import { useSelector } from 'react-redux'
+import { ReactComponent as Logo } from '../../assets/crown.svg'
+
+import { useDispatch } from 'react-redux'
+import { signOutStart } from '../../store/user/userAction'
+
+import { selectCurrentUser } from '../../store/user/userSelector'
+
+import Cart from '../Cart/Cart'
+
+import {
+  OptionLink,
+  NavContainer,
+  LogoContainer,
+  OptionContainer,
+} from './NavStyles'
+
+export default function Nav() {
+  const dispatch = useDispatch()
+  const currentUser = useSelector(selectCurrentUser)
+
+  return (
+    <NavContainer>
+      <LogoContainer to="/">
+        <Logo />
+      </LogoContainer>
+
+      <OptionContainer>
+        <OptionLink to="/shop">SHOP</OptionLink>
+        <OptionLink to="/">CONTACT</OptionLink>
+        {currentUser ? (
+          <OptionLink as="div" onClick={() => dispatch(signOutStart())}>
+            SIGN OUT
+          </OptionLink>
+        ) : (
+          <OptionLink className="option" to="/auth">
+            SIGN IN
+          </OptionLink>
+        )}
+        <Cart />
+      </OptionContainer>
+    </NavContainer>
+  )
+}
