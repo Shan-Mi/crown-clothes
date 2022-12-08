91bb713999919382396da737edcafca61ea04d3c
Shan-Mi <shan.mi.fanfan@gmail.com>
2022-12-08 21:21:23 +0100

    Add SignIn and its style

---
+++ src/Components/SignIn/SignIn.jsx
@@ -0,0 +1,87 @@
+import { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import {
+  googleSignInStart,
+  emailSignInStart,
+} from '../../store/user/userAction'
+
+import Input from '../Input/Input'
+import Button, { BUTTON_TYPE_CLASSES } from '../Button/Button'
+
+import { SignInContainer, ButtonsContainer } from './SignInStyles'
+
+const defaultFormFields = {
+  email: '',
+  password: '',
+}
+
+export default function SignIn() {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const [formFields, setFormFields] = useState(defaultFormFields)
+  const { email, password } = formFields
+
+  const resetFormFields = () => setFormFields(defaultFormFields)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    try {
+      dispatch(emailSignInStart(email, password))
+      resetFormFields()
+      setTimeout(() => {
+        navigate('/')
+      }, 500)
+    } catch (error) {
+      console.error('user sign in failed', error)
+    }
+  }
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setFormFields({ ...formFields, [name]: value })
+  }
+
+  const handleOnClick = () => {
+    dispatch(googleSignInStart())
+    setTimeout(() => {
+      navigate('/')
+    }, 500)
+  }
+
+  return (
+    <SignInContainer>
+      <h2>I already have an account</h2>
+      <p>Sign In with your email and password</p>
+      <form onSubmit={handleSubmit}>
+        <Input
+          handleChange={handleChange}
+          type="email"
+          name="email"
+          value={email}
+          label="Email"
+          required
+        />
+        <Input
+          handleChange={handleChange}
+          type="password"
+          name="password"
+          value={password}
+          label="Password"
+          required
+        />
+        <ButtonsContainer>
+          <Button style={{ marginRight: '20px' }} type="submit">
+            sign in
+          </Button>
+          <Button
+            buttonType={BUTTON_TYPE_CLASSES.google}
+            onClick={handleOnClick}>
+            sign in with google
+          </Button>
+        </ButtonsContainer>
+      </form>
+    </SignInContainer>
+  )
+}
---
+++ src/Components/SignIn/SignInStyles.js
@@ -0,0 +1,45 @@
+import styled from 'styled-components'
+
+export const SignInContainer = styled.div`
+  width: 420px;
+  display: flex;
+  flex-direction: column;
+
+  h2 {
+    margin: 10px 0;
+  }
+
+  @media screen and (max-width: 420px) {
+    width: 220px;
+    p,
+    h2 {
+      text-align: center;
+    }
+  }
+
+  @media (min-width: 450px) and (max-width: 1024px) {
+    p,
+    h2 {
+      text-align: center;
+    }
+
+    padding-bottom: 32px;
+  }
+`
+
+export const ButtonsContainer = styled.div`
+  display: flex;
+
+  button {
+    margin: 0 auto;
+    margin-bottom: 8px;
+  }
+
+  @media screen and (max-width: 420px) {
+    flex-direction: column;
+
+    button {
+      width: 100%;
+    }
+  }
+`
