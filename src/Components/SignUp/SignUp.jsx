8ea2b6978c03342cb1e8d42c1b6619a81d16e674
Shan-Mi <shan.mi.fanfan@gmail.com>
2022-12-08 21:21:39 +0100

    Add SignUp and its style

---
+++ src/Components/SignUp/SignUp.jsx
@@ -0,0 +1,91 @@
+import { useState } from 'react'
+import { useDispatch } from 'react-redux'
+
+import { signUpStart } from '../../store/user/userAction'
+
+import { toast } from 'react-toastify'
+
+import Input from '../Input/Input'
+import Button from '../Button/Button'
+
+import { SignUpContainer } from './SignUpStyles'
+
+const defaultFormFields = {
+  displayName: '',
+  email: '',
+  password: '',
+  confirmPassword: '',
+}
+
+export default function SignUp() {
+  const dispatch = useDispatch()
+  const [formFields, setFormFields] = useState(defaultFormFields)
+
+  const { displayName, email, password, confirmPassword } = formFields
+
+  const resetFormFields = () => setFormFields(defaultFormFields)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    if (password !== confirmPassword) {
+      toast.error('Password and Confirm Password must be same.')
+      return
+    }
+
+    try {
+      dispatch(signUpStart(email, password, displayName))
+      resetFormFields()
+    } catch (err) {
+      toast.error(`${err.message}`)
+    }
+  }
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setFormFields({ ...formFields, [name]: value })
+  }
+
+  return (
+    <SignUpContainer>
+      <h2>I do not have an account</h2>
+      <p>Sign up with your email and password</p>
+
+      <form onSubmit={handleSubmit}>
+        <Input
+          handleChange={handleChange}
+          type="text"
+          name="displayName"
+          value={displayName}
+          label="Display Name"
+          required
+        />
+        <Input
+          handleChange={handleChange}
+          type="email"
+          name="email"
+          value={email}
+          label="Email"
+          required
+        />
+        <Input
+          handleChange={handleChange}
+          type="password"
+          name="password"
+          value={password}
+          label="Password"
+          required
+        />
+        <Input
+          handleChange={handleChange}
+          type="password"
+          name="confirmPassword"
+          value={confirmPassword}
+          label="Confirm Password"
+          required
+        />
+        <Button type="submit">sign up</Button>
+      </form>
+    </SignUpContainer>
+  )
+}
---
+++ src/Components/SignUp/SignUpStyles.js
@@ -0,0 +1,37 @@
+import styled from 'styled-components'
+
+export const SignUpContainer = styled.div`
+  width: 420px;
+  display: flex;
+  flex-direction: column;
+
+  h2 {
+    margin: 10px 0;
+  }
+
+  button {
+    margin: 0 auto;
+  }
+
+  @media screen and (max-width: 420px) {
+    width: 220px;
+
+    button {
+      width: 100%;
+    }
+
+    p,
+    h2 {
+      text-align: center;
+    }
+  }
+
+  @media screen and (max-width: 1024px) {
+    p,
+    h2 {
+      text-align: center;
+    }
+
+    padding-top: 50px;
+  }
+`
