4a8d8d836eccc404850de0b74c14c65303c25036
Shan-Mi <shan.mi.fanfan@gmail.com>
2022-12-08 21:32:10 +0100

    Add DirectoryItem and Directory

---
+++ src/Components/Directory/Directory.jsx
@@ -0,0 +1,13 @@
+import DirectoryItem from '../DirectoryItem/DirectoryItem'
+
+import { DirectoryContainer } from './DirectoryStyles'
+
+export default function Directory({ categories }) {
+  return (
+    <DirectoryContainer>
+      {categories.map((category) => (
+        <DirectoryItem key={category.id} category={category} />
+      ))}
+    </DirectoryContainer>
+  )
+}
---
+++ src/Components/DirectoryItem/DirectoryItem.jsx
@@ -0,0 +1,23 @@
+import { useNavigate } from 'react-router-dom'
+import {
+  ContentTitle,
+  ContentSubtitle,
+  ContentContainer,
+  DirectoryItemContainer,
+  BackgroundImageContainer,
+} from './DirectoryItemStyles'
+
+export default function DirectoryItem({ category }) {
+  const navigate = useNavigate()
+  const { title, imageUrl, size, route } = category
+
+  return (
+    <DirectoryItemContainer onClick={() => navigate(route)} size={size}>
+      <BackgroundImageContainer imageUrl={imageUrl} />
+      <ContentContainer>
+        <ContentTitle>{title.toUpperCase()}</ContentTitle>
+        <ContentSubtitle>SHOP NOW</ContentSubtitle>
+      </ContentContainer>
+    </DirectoryItemContainer>
+  )
+}
