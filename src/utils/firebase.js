03d24390d83d79dad35eb71c5a1f864e89110638
Shan-Mi <shan.mi.fanfan@gmail.com>
2022-12-08 21:06:26 +0100

    Add utils to connect to firebase and stripe

---
+++ src/utils/firebase.js
@@ -0,0 +1,145 @@
+import { initializeApp } from 'firebase/app'
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc,
+  collection,
+  writeBatch,
+  query,
+  getDocs,
+} from 'firebase/firestore'
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signInWithRedirect,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+} from 'firebase/auth'
+
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyD4FzJzLcr1J6-vqix-EsTSHZLdhUW2poM',
+//   authDomain: 'crown-clothing-399f7.firebaseapp.com',
+//   projectId: 'crown-clothing-399f7',
+//   storageBucket: 'crown-clothing-399f7.appspot.com',
+//   messagingSenderId: '24535546838',
+//   appId: '1:24535546838:web:bd6c1bc1760a50e0c1c277',
+// }
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBmt190S13Po1z2n_cz03BQTMoFKgRqhxs',
+  authDomain: 'crown-db-59102.firebaseapp.com',
+  databaseURL: 'https://crown-db-59102.firebaseio.com',
+  projectId: 'crown-db-59102',
+  storageBucket: 'crown-db-59102.appspot.com',
+  messagingSenderId: '211607986071',
+  appId: '1:211607986071:web:0dfc930ec7343c2f9eaed2',
+  measurementId: 'G-VDWWFJBJER',
+}
+
+const firebaseApp = initializeApp(firebaseConfig)
+
+const auth = getAuth(firebaseApp)
+
+const googleProvider = new GoogleAuthProvider()
+googleProvider.setCustomParameters({ prompt: 'select_account' })
+
+const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider)
+const signInWithGoogleRedirect = () => signInWithRedirect(auth, googleProvider)
+
+const db = getFirestore(firebaseApp)
+
+const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+  const collectionRef = collection(db, collectionKey)
+  const batch = writeBatch(db)
+
+  objectsToAdd.forEach((object) => {
+    const docRef = doc(collectionRef, object.title.toLowerCase())
+    batch.set(docRef, object)
+  })
+
+  await batch.commit()
+  // console.log('batch commit successful');
+}
+
+const getCategoriesAndDocuments = async () => {
+  const collectionRef = collection(db, 'categories')
+  const q = query(collectionRef)
+
+  const querySnapshot = await getDocs(q)
+  return querySnapshot.docs.map((doc) => doc.data())
+}
+
+const createUserDocumentFromAuth = async (
+  userAuth,
+  additionalInformation = {}
+) => {
+  if (!userAuth) return
+
+  // existing document reference
+  const userDocRef = doc(db, 'users', userAuth.uid)
+
+  // user data file object
+  const userSnapshot = await getDoc(userDocRef)
+
+  // if user exists
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth
+    const createdAt = new Date()
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+        ...additionalInformation,
+      })
+    } catch (error) {
+      console.log('error creating the user: ', error.message)
+    }
+  }
+
+  return userSnapshot
+}
+
+const createAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return
+  return await createUserWithEmailAndPassword(auth, email, password)
+}
+
+const signInAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !email) return
+  return await signInWithEmailAndPassword(auth, email, password)
+}
+
+const signOutUser = async () => await signOut(auth)
+
+const getCurrentUser = () => {
+  return new Promise((resolve, reject) => {
+    const unsubscribe = onAuthStateChanged(
+      auth,
+      (userAuth) => {
+        unsubscribe()
+        resolve(userAuth)
+      },
+      reject
+    )
+  })
+}
+
+export {
+  db,
+  auth,
+  signInWithGooglePopup,
+  signInWithGoogleRedirect,
+  createUserDocumentFromAuth,
+  signInAuthUserWithEmailAndPassword,
+  createAuthUserWithEmailAndPassword,
+  signOutUser,
+  addCollectionAndDocuments,
+  getCategoriesAndDocuments,
+  getCurrentUser,
+}
