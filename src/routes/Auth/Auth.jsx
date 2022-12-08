4b4043616704f6d78bd4879573a220be6f9d687a
Shan-Mi <shan.mi.fanfan@gmail.com>
2022-12-08 21:22:02 +0100

    Add route auth and place signin/signup here

---
+++ src/routes/Auth/Auth.jsx
@@ -0,0 +1,13 @@
+import SignIn from '../../Components/SignIn/SignIn'
+import SignUp from '../../Components/SignUp/SignUp'
+
+import { AuthContainer } from './AuthStyles'
+
+export default function Auth() {
+  return (
+    <AuthContainer>
+      <SignIn />
+      <SignUp />
+    </AuthContainer>
+  )
+}
---
+++ src/routes/Auth/AuthStyles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const AuthContainer = styled.div`
+  max-width: 920px;
+  margin: 30px auto;
+  display: flex;
+  justify-content: space-between;
+
+  @media screen and (max-width: 1024px) {
+    flex-direction: column;
+    align-items: center;
+  }
+`
