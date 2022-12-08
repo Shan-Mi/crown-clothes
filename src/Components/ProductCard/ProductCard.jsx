8fefa8b24aa46a89772a57041728ce410a39586c
Shan-Mi <shan.mi.fanfan@gmail.com>
2022-12-08 21:27:40 +0100

    Add ProductCard and its style

---
+++ src/Components/ProductCard/ProductCard.jsx
@@ -0,0 +1,29 @@
+import { useDispatch, useSelector } from 'react-redux'
+import { addItemToCart } from '../../store/cart/cartAction'
+import { selectCartItems } from '../../store/cart/cartSelector'
+
+import Button from '../Button/Button'
+
+import { Name, Price, Footer, ProductCartContainer } from './ProductCardStyles'
+
+export default function ProductCard({ product }) {
+  const dispatch = useDispatch()
+  const cartItems = useSelector(selectCartItems)
+
+  const { imageUrl, name, price } = product
+
+  return (
+    <ProductCartContainer>
+      <img src={imageUrl} alt={`${name}`} />
+      <Footer>
+        <Name>{name}</Name>
+        <Price>${price}</Price>
+      </Footer>
+      <Button
+        onClick={() => dispatch(addItemToCart(cartItems, product))}
+        buttonType="inverted">
+        add to cart
+      </Button>
+    </ProductCartContainer>
+  )
+}
