a57d7c75625f2ea70bc424476e837cbb7c7a85b8
Shan-Mi <shan.mi.fanfan@gmail.com>
2022-12-08 21:29:01 +0100

    Add CategoryPreview and its style

---
+++ src/Components/CategoryPreview/CategoryPreview.jsx
@@ -0,0 +1,27 @@
+import { useNavigate } from 'react-router-dom'
+import {
+  Title,
+  Preview,
+  CategoryPreviewContainer,
+} from './CategoryPreviewStyles'
+
+import ProductCard from '../ProductCard/ProductCard'
+
+export default function CategoryPreview({ title, products }) {
+  const navigate = useNavigate()
+
+  return (
+    <CategoryPreviewContainer>
+      <Title onClick={() => navigate(`/shop/${title}`)}>
+        {title.toUpperCase()}
+      </Title>
+      <Preview>
+        {products
+          .filter((_, idx) => idx < 4)
+          .map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+      </Preview>
+    </CategoryPreviewContainer>
+  )
+}
---
+++ src/Components/CategoryPreview/CategoryPreviewStyles.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components'
+
+export const CategoryPreviewContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 30px;
+
+  @media screen and (max-width: 800px) {
+    align-items: center;
+  }
+`
+
+export const Title = styled.h2`
+  font-size: 1.8rem;
+  margin-bottom: 25px;
+`
+
+export const Preview = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 40px;
+
+  @media screen and (max-width: 800px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media (min-width: 820px) and (max-width: 1024px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+`
