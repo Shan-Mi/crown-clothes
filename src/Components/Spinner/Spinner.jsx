3440c1144fb07f6391ed53492884c3baae4e079d
Shan-Mi <shan.mi.fanfan@gmail.com>
2022-12-08 21:28:30 +0100

    Add Spinner for rendering loading

---
+++ src/Components/Spinner/Spinner.jsx
@@ -0,0 +1,9 @@
+import { SpinnerOverlay, SpinnerContainer } from './SpinnerStyles'
+
+export default function Spinner() {
+  return (
+    <SpinnerOverlay>
+      <SpinnerContainer />
+    </SpinnerOverlay>
+  )
+}
