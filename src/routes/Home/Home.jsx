4f948b605100c84a939752473479670dfb6f0058
Shan-Mi <shan.mi.fanfan@gmail.com>
2022-12-08 21:57:26 +0100

    Render Homepage with Directory

---
+++ src/routes/Home/Home.jsx
@@ -0,0 +1,12 @@
+import categories from '../../data/categories'
+import Directory from '../../Components/Directory/Directory'
+
+import { HomeContainer } from './HomeStyles'
+
+export default function Home() {
+  return (
+    <HomeContainer>
+      <Directory categories={categories} />
+    </HomeContainer>
+  )
+}
---
+++ src/routes/Home/HomeStyles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export const HomeContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  @media screen and (max-width: 500px) {
+    height: 90vh;
+    justify-content: center;
+  }
+
+  @media (min-width: 512px) and (max-width: 800px) {
+    height: 75vh;
+    justify-content: center;
+  }
+`
