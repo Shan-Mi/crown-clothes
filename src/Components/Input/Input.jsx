7abfbf5cf70639bd8c758da9a3cd85670da8f26d
Shan-Mi <shan.mi.fanfan@gmail.com>
2022-12-08 21:26:17 +0100

    Add Input and its style

---
+++ src/Components/Input/Input.jsx
@@ -0,0 +1,29 @@
+import './styles.scss'
+
+export default function Input({
+  name,
+  type,
+  label,
+  value,
+  required,
+  handleChange,
+}) {
+  return (
+    <div className="input">
+      <input
+        className="inputFeild"
+        onChange={handleChange}
+        type={type}
+        name={name}
+        value={value}
+        required={required}
+      />
+
+      {label ? (
+        <label className={`${value.length ? 'shrink' : ''} label`}>
+          {label}
+        </label>
+      ) : null}
+    </div>
+  )
+}
