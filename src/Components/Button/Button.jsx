d150fa4234e88effed30a5350326f00e35e30533
Shan-Mi <shan.mi.fanfan@gmail.com>
2022-12-08 21:14:22 +0100

    Add Button and its style

---
+++ src/Components/Button/Button.jsx
@@ -0,0 +1,33 @@
+import {
+  BaseButton,
+  GoogleSignInButton,
+  InvertedButton,
+  LoadingSpinner,
+} from './ButttonStyles'
+
+export const BUTTON_TYPE_CLASSES = {
+  base: 'base',
+  inverted: 'inverted',
+  google: 'google-sign-in',
+}
+
+export const getButton = (buttonType = BUTTON_TYPE_CLASSES.base) =>
+  ({
+    [BUTTON_TYPE_CLASSES.base]: BaseButton,
+    [BUTTON_TYPE_CLASSES.inverted]: InvertedButton,
+    [BUTTON_TYPE_CLASSES.google]: GoogleSignInButton,
+  }[buttonType])
+
+export default function Button({
+  children,
+  buttonType,
+  isLoading = false,
+  ...otherProps
+}) {
+  const CustomButton = getButton(buttonType)
+  return (
+    <CustomButton disabled={isLoading} {...otherProps}>
+      {isLoading ? <LoadingSpinner /> : children}
+    </CustomButton>
+  )
+}
