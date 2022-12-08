2d3a30ef06655a9b76622866cdbdb2cb8c5ae54c
Shan-Mi <shan.mi.fanfan@gmail.com>
2022-12-08 21:58:44 +0100

    Add all components to app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { Routes, Route } from 'react-router-dom'
 
-function App() {
+import { checkUserSession } from './store/user/userAction'
+
+import Nav from './Components/Nav/Nav'
+
+import Home from './routes/Home/Home'
+import Shop from './routes/Shop/Shop'
+import Auth from './routes/Auth/Auth'
+import Checkout from './routes/Checkout/Checkout'
+
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
+import { GlobalStyle } from './globalStyles'
+
+export default function App() {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(checkUserSession())
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+      <GlobalStyle />
+      <Nav />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="shop/*" element={<Shop />} />
+        <Route path="auth" element={<Auth />} />
+        <Route path="checkout" element={<Checkout />} />
+      </Routes>
 
-export default App;
+      <ToastContainer
+        position="top-center"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={true}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss={false}
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+    </>
+  )
+}
