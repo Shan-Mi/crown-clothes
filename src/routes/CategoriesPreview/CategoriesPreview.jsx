d6e75d24d7b0c5b14cb87a472ac995b1d9091a9e
Shan-Mi <shan.mi.fanfan@gmail.com>
2022-12-08 21:30:31 +0100

    Add CategoriesPreview

---
+++ src/routes/CategoriesPreview/CategoriesPreview.jsx
@@ -0,0 +1,29 @@
+import { useSelector } from 'react-redux'
+import {
+  selectIsLoading,
+  selectCategoriesMap,
+} from '../../store/category/categorySelector'
+
+import Spinner from '../../Components/Spinner/Spinner'
+import CategoryPreview from '../../Components/CategoryPreview/CategoryPreview'
+
+export default function CategoriesPreview() {
+  const isLoading = useSelector(selectIsLoading)
+  const categoriesMap = useSelector(selectCategoriesMap)
+
+  return (
+    <>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        Object.keys(categoriesMap).map((title) => {
+          const products = categoriesMap[title]
+
+          return (
+            <CategoryPreview key={title} title={title} products={products} />
+          )
+        })
+      )}
+    </>
+  )
+}
