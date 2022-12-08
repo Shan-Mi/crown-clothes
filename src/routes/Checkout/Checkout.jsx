649ca9f464b1718f52ad6c45bf61cfd28f93fcb1
Shan-Mi <shan.mi.fanfan@gmail.com>
2022-12-08 21:55:09 +0100

    Add Checkout page

---
+++ src/routes/Checkout/Checkout.jsx
@@ -0,0 +1,46 @@
+import { useSelector } from 'react-redux'
+import { selectCartItems, selectCartTotal } from '../../store/cart/cartSelector'
+
+import Payment from '../../Components/Payment/Payment'
+import CheckoutItem from '../../Components/CheckoutItem/CheckoutItem'
+
+import {
+  CartTotal,
+  CheckoutHeader,
+  CheckoutContainer,
+  CheckoutHeaderBlock,
+} from './CheckoutStyles'
+
+export default function Checkout() {
+  const cartItems = useSelector(selectCartItems)
+  const cartTotal = useSelector(selectCartTotal)
+
+  return (
+    <CheckoutContainer>
+      <CheckoutHeader>
+        <CheckoutHeaderBlock>
+          <span>Product</span>
+        </CheckoutHeaderBlock>
+        <CheckoutHeaderBlock>
+          <span>Description</span>
+        </CheckoutHeaderBlock>
+        <CheckoutHeaderBlock>
+          <span>Quantity</span>
+        </CheckoutHeaderBlock>
+        <CheckoutHeaderBlock>
+          <span>Price</span>
+        </CheckoutHeaderBlock>
+        <CheckoutHeaderBlock>
+          <span>Remove</span>
+        </CheckoutHeaderBlock>
+      </CheckoutHeader>
+
+      {cartItems.map((cartItem) => (
+        <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+      ))}
+
+      <CartTotal>Total : ${cartTotal}</CartTotal>
+      <Payment />
+    </CheckoutContainer>
+  )
+}
